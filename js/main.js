1f73a3aefda34017d89440938367caabe5dcb4b6
Sergey <serzh-kudryashov@mail.ru>
2021-02-03 18:07:31 +0300

    Написал две функции

---
+++ js/main.js
@@ -0,0 +1,26 @@
+let randomNumber = function (min, max) { 
+    if(min === max){
+
+        return Math.random() * (max - min) + min;
+    } else {
+
+        return Math.floor(Math.random() * (max - min + 1)) + min
+    }
+
+   
+
+};
+
+<a href = "http://espressocode.top/how-to-generate-random-number-in-given-range-using-javascript/"></a>
+
+ function coment (str, maxLenght){
+    if(str.lenght > maxLenght){
+        return
+    }
+    
+}
+
+
+
+
+
