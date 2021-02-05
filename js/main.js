7cace7be2dcf704de1580441cf2fd208bb6a3b7b
Sergey <serzh-kudryashov@mail.ru>
2021-02-05 07:45:18 +0300

    В сотый раз переделал задание

--- js/main.js
+++ js/main.js
@@ -1,24 +1,30 @@
 "use strict";
 
-let randomNumber = function (min, max) {
+let randomNumber = (min, max) => {
     if(min > max){
-        return Math.floor(Math.random() * (max + 1))  // функция возвращает целое число из переданного диапазона включительно 
 
-    }else {
+        return false 
 
-       return min + Math.floor( Math.random() * (max + 1 - min)) 
+    } else {
+
+        return Math.floor(Math.random() * (max + 1))
+        
     }
-}
-    
-/*Проверил вторую функцию отрицательныx значений не получил, если только не передать в параметры функции отрицательное число. Хотел сделать проверку на отрицательное число методом Math.abs(), но понимания не хватило, как доделать до конца и подумал имеется ли вообще в этом смысл. Будут ли такие параметры в эту функцию передаваться.  */
+};
+
+
        
 
 
 
 
- function checkLength (str, maxLenght) {
-    return (str.lenght > maxLenght) ? true : false
- }
+ let checkLength = (str, maxLength) => {
+
+    return (str.lenght > maxLength) ? true : false;
+
+ };
+    
+ 
 
 
 
