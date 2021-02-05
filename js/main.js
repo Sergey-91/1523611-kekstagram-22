88b36f182c6213a36958ab4db201fa9bb9899d2a
Sergey <serzh-kudryashov@mail.ru>
2021-02-05 04:06:54 +0300

    Написал новые функции и добавил строгий режим

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
-let randomNumber = function (min, max) { 
-    if(min === max){
+"use strict";
 
-        return Math.random() * (max - min) + min;
-    } else {
+let randomNumber = function (min, max) {
+    if(min > max){
+        return Math.floor(Math.random() * (max + 1))  // функция возвращает целое число из переданного диапазона включительно 
 
-        return Math.floor(Math.random() * (max - min + 1)) + min
+    }else {
+
+       return min + Math.floor( Math.random() * (max + 1 - min)) 
     }
+}
+    
+/*Проверил вторую функцию отрицательныx значений не получил, если только не передать в параметры функции отрицательное число. Хотел сделать проверку на отрицательное число методом Math.abs(), но понимания не хватило, как доделать до конца и подумал имеется ли вообще в этом смысл. Будут ли такие параметры в эту функцию передаваться.  */
+       
 
-   
 
-};
 
-<a href = "http://espressocode.top/how-to-generate-random-number-in-given-range-using-javascript/"></a>
 
  function checkLength (str, maxLenght) {
     return (str.lenght > maxLenght) ? true : false
